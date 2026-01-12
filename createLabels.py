00c68093370ddbf1b2773d99aadbb1c0d629e240
pong02 <pongzhongzhe@gmail.com>
2026-01-12 11:30:39 +0800

    removing sender info

--- createLabels.py
+++ createLabels.py
@@ -27,11 +27,11 @@ WAREHOUSE_CONFIG = {
         "postcode": "3810",
         "country": "AU",
         "label_sender_block": [
-            "*IF UNDELIVERED RETURN TO*",
-            "GrabNest",
-            "6 Envision Cl",
-            "Pakenham",
-            "VIC 3810"
+            # "*IF UNDELIVERED RETURN TO*",
+            # "GrabNest",
+            # "6 Envision Cl",
+            # "Pakenham",
+            # "VIC 3810"
             # " ",
             # " ",
             # " ",
@@ -44,17 +44,17 @@ WAREHOUSE_CONFIG = {
     "2": {
         "sender_name": "NexGen",
         "sender_email": "nexgenau23@gmail.com",
-        "address_line1": "14 Derby Road",
+        "address_line1": "12 Derby Road",
         "suburb": "Caulfield East",
         "state_name": "VIC",
         "postcode": "3145",
         "country": "AU",
         "label_sender_block": [
-            "*IF UNDELIVERED RETURN TO*",
-            "NexGenAU",
-            "14 Derby Road",
-            "Caulfield East",
-            "VIC 3145"
+            # "*IF UNDELIVERED RETURN TO*",
+            # "NexGenAU",
+            # "12 Derby Road",
+            # "Caulfield East",
+            # "VIC 3145"
         ],
         "quote_pickup_suburb": "CARNEGIE",
         "quote_pickup_postcode": "3163"
