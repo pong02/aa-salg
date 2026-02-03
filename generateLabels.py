0fd4fea951adcd1b09ee05b0d4df3dcee1f0e92a
pong02 <pongzhongzhe@gmail.com>
2026-02-03 16:24:35 +0800

    Revert "ebay field update"
    This reverts commit 8d7b6f2d562e173fb08c6036e17ce81e72ade841.

--- generateLabels.py
+++ generateLabels.py
@@ -25,11 +25,11 @@ COLUMN_MAPPING = {
     },
     'ebay': {
         'order number': 'id', 'buyer username': 'buyer_username',
-        'shipping service': 'shipping_method', 'quantity': 'Quantity',
-        'custom label': 'custom_label', 'ship to name': 'rname',
-        'ship to address 1': 'address1', 'ship to address 2': 'address2',
-        'ship to city': 'city', 'ship to state': 'state',
-        'ship to zip': 'zip',
+        'postage service': 'shipping_method', 'quantity': 'Quantity',
+        'custom label': 'custom_label', 'post to name': 'rname',
+        'post to address 1': 'address1', 'post to address 2': 'address2',
+        'post to city': 'city', 'post to state': 'state',
+        'post to postal code': 'zip',
         'sold for':'amt'
     },
     'catch': {
